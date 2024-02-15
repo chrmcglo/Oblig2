a447c8dda38b3f3a0873fe0cc1eaad1472e5a078
Christian Mc <chrmcglo@icloud.com>
2024-02-15 08:44:36 +0100

    Fikset oppgave 1

--- Oblig2.cpp
+++ Oblig2.cpp
@@ -9,7 +9,7 @@
 std::optional<int> find_minimum(const std::vector<int>& data);
 // template 
 template <typename T>
-T utregning(const T x, const T y)
+T utregning(const T &x, const T &y)
 {
     return x + y;
 }
@@ -17,8 +17,9 @@ T utregning(const T x, const T y)
 
 // Oppgave 1. lag en vector med verdier, send vector til funksjon find_minimum, returner minste verdi hvis ingen nullopt. 
 void oppgave1() {
-    //std::cout << " funksjon oppgave 1. OK\n";
-    std::vector<int> vec3{};// kræsj hvis jeg prøver å sende vec3 inn i funksjon find_minumun uten verdier.
+    std::cout << " funksjon oppgave 1. OK\n";
+    std::vector<int> vec3{ 2,4,5,34,3 };;// kræsj hvis jeg prøver å sende vec3 inn i funksjon find_minumun uten verdier.
+    std::cout << "Vektor 2,4,5,34,3\n";
     auto minVal = find_minimum(vec3);
     if (minVal.has_value()) {
         std::cout << "minste verdi i vector: " << minVal.value();
@@ -36,9 +37,9 @@ void oppgave2() {
     int b{};
     double d1{};
     double d2{};
-    std::cout << "oppgi to heltall\n";
+    std::cout << "oppgi to Heltall\n";
     std::cin >> a >> b;
-    std::cout << "Oppgi to flyttall\n";
+    std::cout << "Oppgi to Desimaltall\n";
     std::cin >> d1 >> d2;
 
     std::cout << "heltall utregning: " << utregning(a, b) << std::endl;
@@ -64,7 +65,7 @@ std::optional<int> find_minimum(const std::vector<int>& data){
 int main()
 {
     int valg;
-    std::cout << "Hvilken oppgave?\n 1. Vectorer og Nullopt-optional\n 2. Regner ut summen og pointers-Templates\n 3. Antall i en liste. \n Valg:";
+    std::cout << "Hvilken oppgave?\n 1. Oppgave 1\n 2. Oppgave 2\n 3. Oppgave 3\n Valg:";
     std::cin >> valg;
     switch (valg) {
     case 1:
@@ -77,7 +78,7 @@ int main()
         oppgave3();
         break;
     default:
-        std::cout << "Ugyldig valg.Programmer avsluttes\n";
+        std::cout << "Ugyldig valg.Programmet avsluttes\n";
 
     }
     
