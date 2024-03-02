7b23d582c24e10d9cfb41b867ad5fedf3c010a9c
chrmc <chrmcglo@icloud.com>
2024-03-02 16:08:56 +0100

    Opprydding, Testet OK.

--- Module.ixx
+++ Module.ixx
@@ -1,15 +1,32 @@
 export module my_module;
 import <iostream>;
 import <vector>;
-export {
-	template <typename T>
-	T utregning(const T& x, const T& y) {
-		return x + y;
-	}
+import <optional>;
+
+
+export{
+    // Oppgave 1
+   std::optional<int> find_minimum(const std::vector<int>& data) {
+        if (data.empty()) return std::nullopt;  // uten nullopt blir det kræsj!
+        else {
+            int start{ data[0] };
+            for (auto value : data) {
+                if (start > value) {
+                    start = value;
+                }
+            }
+            return start;
+        }
+    }
+    // Oppgave 2
+    template <typename T>
+    T utregning(const T& x, const T& y) {
+        return x + y;
+    }
 
     template <typename T>
     auto count(const std::vector<T>& x) { // Kan ikke ha "T" hvis vi ikke returnere noe.
-    int size = x.size();
+        int size = x.size();
         for (int i = 0; i < size - 1; i++) {
             auto value = x[i];
             if (value == x[i + 1]) {
@@ -17,8 +34,9 @@ export {
             }
         }
     }
+    //Oppgave 3
     template <typename T>
-    auto count(const T* x, size_t size) {
+    T count(const T* x, size_t size) {
         for (size_t i = 0; i < size - 1; i++) {
             if (x[i] == x[i + 1]) {
                 return x[i];
--- Oblig2.cpp
+++ Oblig2.cpp
@@ -2,66 +2,44 @@
 //
 import my_module;
 #include <iostream>
-#include <optional>
 #include <span>
 #include <vector>
-//Decleration
-std::optional<int> find_minimum(const std::vector<int>& data);
 
-// Oppgave 1. lag en vector med verdier, send vector til funksjon find_minimum, returner minste verdi hvis ingen nullopt. 
+// Oppgave 1 Finne minste verdi i en vector.  
 void oppgave1() {
     std::vector<int> vec3{ 2,4,5,34,3 };// kræsj hvis jeg prøver å sende vec3 inn i funksjon find_minumun uten verdier.
-    for (int value : vec3) { 
-    }
     auto minVal = find_minimum(vec3);
+
     if (minVal.has_value()) {
-        std::cout << "minste verdi i vector: 2,4,5,34,3 er: " << minVal.value() << std::endl;
+        std::cout << "Minste verdi i vec3(2, 4, 5, 34, 3): " << minVal.value() << std::endl;
     }
     else {
-        std::cout << "Tom vector!\n";
+        std::cout << "Viktor vector er tom!\n";
     }
+  }
 
-}
-// bruk av Template
-
+// Oppgave 3 bruk av Template
 void oppgave2() {
-    int a{};
-    int b{};
-    double d1{};
-    double d2{};
-    std::cout << "oppgi to Heltall\n";
-    std::cin >> a >> b;
-    std::cout << "Oppgi to Desimaltall\n";
-    std::cin >> d1 >> d2;
-    std::cout << "Heltall utregning: " << utregning(a, b) << std::endl;
-    std::cout << "Desimaltall utregning: " << utregning(d1, d2);
+    int a,b{};
+    double d1,d2{};
+    std::cout << "Oppgi to Heltall\n"; std::cin >> a >> b;   // Sender to Heltall til "a" og "b"
+    std::cout << "Oppgi to Desimaltall\n"; std::cin >> d1 >> d2; // Sender to Desimaltall til "a" og "b"
+    std::cout << "Heltall utregning: " << utregning(a, b) << std::endl;  // Utfører beregning ved bruk av template og module. 
+    std::cout << "Desimaltall utregning: " << utregning(d1, d2) << std::endl; // Utfører beregning ved bruk av template og module. 
 }
 void oppgave3() {
     std::vector<int> vec4{ 2,3,4,4,34,3 };
     std::vector<double> vec5{ 2.2,3.1,3.1,4.3,3.4,33,3 };
     int myarray[6] = { 1,2,3,4,5,5 };
     std::cout << "Funnet i int vec! like!: " << count(vec4) << std::endl;
-    std::cout << "Funnet i double vec!: " << count(vec5) << std::endl;;
-    std::cout << "Funnet i MyArray!: " << count(myarray, 5) << std::endl;;
+    std::cout << "Funnet i double vec!: " << count(vec5) << std::endl;
+    std::cout << "Funnet i MyArray!: " << count(myarray, 5) << std::endl;
 }
 
-std::optional<int> find_minimum(const std::vector<int>& data){
-    if (data.empty()) return std::nullopt;  // uten nullopt blir det kræsj!
-    int minimum{ data[0] };
-    
-    for (auto value : data) {
-      if (minimum > value) {
-          minimum = value;
-      }
-    }
-    return minimum;
-}
-
-int main()
+void main()
 {
     int valg;
     std::cout << "Hvilken oppgave?\n Oppgave 1\n Oppgave 2\n Oppgave 3\n Valg:";
-    //std::cout << my_function() << std::endl;
     std::cin >> valg;
     switch (valg) {
     case 1:
